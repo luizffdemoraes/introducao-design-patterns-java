e01f61dc835c82b88fd4696df814240f2f48faff
luizffdemoraes <lffm1994@gmail.com>
2025-05-07 23:11:38 -0300

    Aplicando o pattern Chain of Responsibility

---
+++ src/main/java/br/com/alura/loja/desconto/DescontoOrcamentoMaiorQuinhentos.java
@@ -0,0 +1,20 @@
+package br.com.alura.loja.desconto;
+
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class DescontoOrcamentoMaiorQuinhentos extends Desconto{
+
+    public DescontoOrcamentoMaiorQuinhentos(Desconto proximo) {
+        super(proximo);
+    }
+
+    public BigDecimal calcular(Orcamento orcamento) {
+        if (orcamento.getValor().compareTo(new BigDecimal("500")) > 0) {
+            return orcamento.getValor().multiply(new BigDecimal("0.5"));
+        }
+
+        return proximo.calcular(orcamento);
+    }
+}
